24acde6445985f74f10380724cdeb914f031cd2e
Yingyi Luo <yingyi.luo@eecs.northwestern.edu>
2018-05-13 22:50:24 +0000

    data analysis scripts

---
+++ draw_pwr.py
@@ -0,0 +1,51 @@
+import json
+import pandas as pd
+#import libdata
+import os
+import numpy as np
+
+#homedir = '/home/cc'
+tag = 'May8-2018'
+csvdir = os.environ['HOME']
+#apps1 = [['bt.C.x', 'ft.B.x'], ['ft.B.x', 'ua.C.x']]
+apps1 = [['mg.B.x', 'ua.C.x'], ['bt.C.x', 'mg.B.x']] 
+#apps1 = [['cg.C.x', 'ft.B.x']]
+#apps1 = [['lu.C.x', 'ep.D.x']]
+#machines = range(1, 2)
+machines = range(2, 3)
+nodes = range(0, 2)
+fans = ['Fan1A', 'Fan1B', 'Fan2A', 'Fan2B', 'Fan3A', 'Fan3B', 'Fan4A', 'Fan4B', 'Fan5A', 'Fan5B', 'Fan6A', 'Fan6B', 'Fan7A', 'Fan7B']
+appattrs = ['cyc', 'inst', 'llcref', 'llcmiss', 'br', 'brmiss', 'l2lin']
+fanGs = { 'FanGroup1' : ['Fan1A', 'Fan1B', 'Fan2A', 'Fan2B', 'Fan3A', 'Fan3B'], 'FanGroup2' : ['Fan4A', 'Fan4B'], 'FanGroup3' : ['Fan5A', 'Fan5B', 'Fan6A', 'Fan6B', 'Fan7A', 'Fan7B'] }
+
+def getFanPwr(rpm, pwr):
+	return lambda x: pwr * ((x / rpm) ** 3.)
+
+def json2df(jfile):
+	objs = [json.loads(line) for line in jfile]
+	return pd.DataFrame(objs)
+
+def procdf(df):
+	df['irate'] = df['inst'] / df['intv']
+	df['power'] = df['energy'] / (2 ** df['eunit']) / df['intv'] * 1e9
+	df['dtemp'] = df['tpkg'] - df['tpkg'].shift(1)
+	for attr in appattrs:
+		df[attr + '_rate'] = df[attr] / df['intv']
+	for g in fanGs:
+		df[g] = df[fanGs[g]].mean(axis=1)
+	return df
+
+if __name__ == '__main__':
+	for hi in machines:
+		for appPair in apps1:
+			app_fname1 = '%s/exp/data/%s/run-%d/coolr1-1000000-%s-%s-node0-stat.log' % (csvdir, tag, hi, appPair[0], appPair[1])
+			app_fname2 = '%s/exp/data/%s/run-%d/coolr1-1000000-%s-%s-node1-stat.log' % (csvdir, tag, hi, appPair[0], appPair[1])
+			df_app1 = procdf(json2df(open(app_fname1, 'r'))).iloc[0:1800]
+			df_app2 = procdf(json2df(open(app_fname2, 'r'))).iloc[0:1800]
+			#if df_app1.shape[0] < df_app2.shape[0]:
+			#	df_app = df_app2
+			#else:
+			#	df_app = df_app1
+			# get the data for the first half hour
+			df_app1.to_csv('/home/cc/exp/csvdata/run-%d-%s-%s-node0-stat.csv' % (hi, appPair[0], appPair[1]))
+			df_app2.to_csv('/home/cc/exp/csvdata/run-%d-%s-%s-node1-stat.csv' % (hi, appPair[0], appPair[1]))
