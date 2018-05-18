f91cc894d6df0788a1f21fdcd43aa09d8ccc1319
yingyiluo <yingyi.luo@eecs.northwestern.edu>
2018-05-18 13:45:36 -0500

    more complete run

--- parsec_run.sh
+++ parsec_run.sh
@@ -3,7 +3,7 @@ exs=/home/cc/parsec_prebuilt
 
 cat >run-stat.sh <<EOF
 sfile=/sys/class/xstat/stat\$1
-ofile=\$HOME/exp/parsecdata/coolr1-1000000-\${2}-node\${1}-stat.log
+ofile=\$HOME/exp/stats/coolr1-1000000-\${2}-node\${1}-stat.log
 
 rm -f \$ofile
 echo 1 > /sys/class/xstat/reset\$1
@@ -29,45 +29,60 @@ function stopstat() {
   sleep 30s
 }
 
-apps="blackscholes canneal ferret freqmine bodytrack"
+apps="blackscholes canneal ferret freqmine bodytrack bt.C.x cg.C.x ft.B.x sp.D.x mg.B.x ua.C.x lu.C.x dc.B.x"
 declare -A optmap
 optmap[blackscholes]="24 /home/cc/parsec_prebuilt/in_10M.txt black_out"
 optmap[canneal]="24 80000 2000 /home/cc/parsec_prebuilt/2500000.nets 300000"
 optmap[ferret]="/home/cc/parsec_prebuilt/corel/ lsh /home/cc/parsec_prebuilt/queries/ 3000 40 24 ferrer_out"
-optmap[freqmine]="/home/cc/parsec_prebuilt/webdocs_250k.dat 7900"
+optmap[freqmine]="/home/cc/parsec_prebuilt/webdocs_250k.dat 7200"
 optmap[bodytrack]="/home/cc/parsec_prebuilt/sequenceB_261/ 4 261 16000 220 0 24"
+optmap[bt.C.x]="</dev/null"
+optmap[cg.C.x]="</dev/null"
+optmap[ft.B.x]="</dev/null"
+optmap[sp.D.x]="</dev/null"
+optmap[mg.B.x]="</dev/null"
+optmap[ua.C.x]="</dev/null"
+optmap[lu.C.x]="</dev/null"
+optmap[dc.B.x]="</dev/null"
 
 for x in $apps
 do
    for y in $apps
    do
-       if [[ "$x" = "$y" ]];
-       then
-           echo "equal"
-           continue
-       fi
+#       if [[ "$x" = "$y" ]];
+#       then
+#           echo "equal"
+#           continue
+#       fi
        echo "sleep"
        sleep 300s
 
        ts=$(date +%F-%H-%M)
        name=${x}-${y}
-       logfx=/home/cc/exp/parseclog/${x}-${y}-${x}-${ts}.out
-       logfy=/home/cc/exp/parseclog/${x}-${y}-${y}-${ts}.out
-
+       logfx=/home/cc/exp/perf/${x}-${y}-${x}-${ts}.out
+       logfy=/home/cc/exp/perf/${x}-${y}-${y}-${ts}.out
+       errfx=/home/cc/exp/err/${x}-${y}-${x}-${ts}.err
+       errfy=/home/cc/exp/err/${x}-${y}-${y}-${ts}.err
+      
        startstat ${x}-${y}
        
        echo ${x}-on-node0
        echo ${y}-on-node1  
-       (time numactl --cpunodebind=0 --membind=0 $exs/$x ${optmap[$x]}) > $logfx 2>&1 &
+       (time numactl --cpunodebind=0 --membind=0 $exs/$x ${optmap[$x]}) 1>$logfx 2>$errfx &
        xpid=$!
 
-       (time numactl --cpunodebind=1 --membind=1 $exs/$y ${optmap[$y]}) > $logfy 2>&1 &
+       (time numactl --cpunodebind=1 --membind=1 $exs/$y ${optmap[$y]}) 1>$logfy 2>$errfy &
        ypid=$!
 
-       wait $xpid
+       sleep 1600s
+       #wait $xpid
+       if ps -p $xpid > /dev/null
+       then
+          kill -9 $xpid
+       fi
        if ps -p $ypid > /dev/null
        then
-          wait $ypid
+          kill -9 $ypid
        fi
 
        stopstat
